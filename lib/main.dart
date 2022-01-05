97fe4daa7891f5e761cd9fd05d1325562c91059e
Francesco Freddi <francyfre@gmail.com>
2022-01-05 19:51:32 +0100

    Flutter Riverpod - Consumer to read Provider (part 3)

--- lib/main.dart
+++ lib/main.dart
@@ -1,16 +1,16 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 
-
 void main() {
   runApp(
-     const ProviderScope(
+    const ProviderScope(
       child: MyApp(),
     ),
   );
 }
- // Provider have no state! is much simpler
-final myProvider = Provider((ref) => 0);
+
+// Provider have no state! is much simpler
+final myProvider = Provider((ref) => 3);
 
 class MyApp extends StatelessWidget {
   const MyApp({Key? key}) : super(key: key);
@@ -20,7 +20,12 @@ class MyApp extends StatelessWidget {
     return MaterialApp(
       home: Scaffold(
         body: Center(
-          child: Text("some number $someNumber"),
+          child: Consumer(
+            builder: (BuildContext context, WidgetRef ref, Widget? _) {
+              int myValue = ref.watch(myProvider);
+              return Text("some number: $myValue");
+            },
+          ),
         ),
         appBar: AppBar(
           actions: [
