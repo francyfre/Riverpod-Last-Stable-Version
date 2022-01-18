839dc10ca68287231b75c268105a80ed076c14eb
Francesco Freddi <francyfre@gmail.com>
2022-01-18 21:36:06 +0100

    Flutter Riverpod - read().update method to DRY (part 12)

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:riverpod_stable_aspiiire/page1.dart';
-import 'package:riverpod_stable_aspiiire/page2.dart';
 
-// videoLesson: https://www.youtube.com/watch?v=qpfHSWziumM&list=PLzaGtnxLcM7HYt-MhMZ-j0Bmeo4RqPHoS&index=11
+// videoLesson: https://www.youtube.com/watch?v=_YmepxERz1g&list=PLzaGtnxLcM7HYt-MhMZ-j0Bmeo4RqPHoS&index=12
 void main() {
   runApp(
     const ProviderScope(
@@ -12,18 +10,32 @@ void main() {
   );
 }
 
+final providerOfNumber = StateProvider((ref) => 0);
+
 class MyApp extends ConsumerWidget {
   const MyApp({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
+    int myNumber = ref.watch(providerOfNumber);
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      routes: {
-        '/page1': (context) => const Page1(),
-        '/page2': (context) => const Page2(),
-      },
-      initialRoute: '/page1',
+      home: Scaffold(
+        body: Center(
+          child: Text('$myNumber', style: const TextStyle(fontSize: 60)),
+        ),
+        floatingActionButton: FloatingActionButton(
+          onPressed: () {
+            //ref.read(providerOfNumber.notifier).state++;
+            ref
+                .read(providerOfNumber.notifier)
+                .update((prevState) => prevState + 1);
+          },
+          child: const Icon(
+            Icons.plus_one_rounded,
+          ),
+        ),
+      ),
     );
   }
 }
